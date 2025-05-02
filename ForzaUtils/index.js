3b4a7a7f40ccb0bfcb645df98d96377a6f5d29b1
dusanders <dusanders@gmail.com>
2025-05-02 10:10:49 -0500

    Update index.js
    resolve error with missing Buffer definition in React Native

--- ForzaUtils/index.js
+++ ForzaUtils/index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import Main from './Main';
 import {name as appName} from './app.json';
+global.Buffer = require('buffer').Buffer;
 
 AppRegistry.registerComponent(appName, () => Main);
