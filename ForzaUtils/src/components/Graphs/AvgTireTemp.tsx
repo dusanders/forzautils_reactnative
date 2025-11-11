674334bd06018699b6ba3f6395d17282c7de2e89
dusanders <dusanders@gmail.com>
2025-11-11 15:04:37 -0600

    re-enable SVG graphs for tire temps

--- ForzaUtils/src/components/Graphs/AvgTireTemp.tsx
+++ ForzaUtils/src/components/Graphs/AvgTireTemp.tsx
@@ -1,8 +1,9 @@
 import React, { useCallback, useMemo } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { useViewModelStore } from "../../context/viewModels/ViewModelStore";
 import { invokeWithTheme, themeService } from "../../hooks/ThemeState";
 import { LineGraph } from "./LIneGraph";
+import { BaseLineGraph } from "./BaseLineGraph";
 
 export interface AvgTireTempProps {
 }
@@ -11,7 +12,8 @@ export function AvgTireTemps(props: AvgTireTempProps) {
   const viewModel = useViewModelStore().tireTemps;
   const theme = themeService().theme;
   const styles = themeStyles();
-
+  
+/** Kept for use with LineGraph component alternative
   const avgAxleLabels = useMemo(() => {
     return [
       {
@@ -56,7 +58,7 @@ export function AvgTireTemps(props: AvgTireTempProps) {
     {
       label: 'Right Rear Tire Temp',
       color: theme.colors.text.primary.onPrimary
-    } 
+    }
   ], [theme]);
   const getTireData = useCallback((tire: 'leftFront' | 'rightFront' | 'leftRear' | 'rightRear') => {
     return {
@@ -65,11 +67,93 @@ export function AvgTireTemps(props: AvgTireTempProps) {
       max: viewModel.tireDataWindow.max,
     }
   }, [viewModel.tireDataWindow]);
+**/
 
   return (
     <ScrollView
-    style={styles.scrollView}>
-      <LineGraph
+      style={styles.scrollView}>
+      <View style={styles.graphContainer}>
+        <BaseLineGraph
+          minY={viewModel.avgTempWindowMin}
+          maxY={viewModel.avgTempWindowMax}
+          dataLength={viewModel.avgTempWindowSize}
+          title="Average Tire Temperatures"
+          data={[
+            {
+              label: 'Front Avg Temp',
+              color: theme.colors.text.primary.onPrimary,
+              data: viewModel.avgTemps.map((point) => point.front),
+            },
+            {
+              label: 'Rear Avg Temp',
+              color: theme.colors.text.secondary.onPrimary,
+              data: viewModel.avgTemps.map((point) => point.rear),
+            },
+          ]}
+        />
+      </View>
+      <View
+        style={styles.graphContainer}>
+        <BaseLineGraph
+          minY={viewModel.tireDataWindow.min}
+          maxY={viewModel.tireDataWindow.max}
+          dataLength={viewModel.tireDataWindow.size}
+          title="Left Front Tire Temperature"
+          data={[
+            {
+              label: 'Left Front Tire Temp',
+              color: theme.colors.text.primary.onPrimary,
+              data: viewModel.tireDataWindow.data.map(t => t.leftFront),
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.graphContainer}>
+        <BaseLineGraph
+          minY={viewModel.tireDataWindow.min}
+          maxY={viewModel.tireDataWindow.max}
+          dataLength={viewModel.tireDataWindow.size}
+          title="Right Front Tire Temperature"
+          data={[
+            {
+              label: 'Right Front Tire Temp',
+              color: theme.colors.text.primary.onPrimary,
+              data: viewModel.tireDataWindow.data.map(t => t.rightFront),
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.graphContainer}>
+        <BaseLineGraph
+          minY={viewModel.tireDataWindow.min}
+          maxY={viewModel.tireDataWindow.max}
+          dataLength={viewModel.tireDataWindow.size}
+          title="Left Rear Tire Temperature"
+          data={[
+            {
+              label: 'Left Rear Tire Temp',
+              color: theme.colors.text.primary.onPrimary,
+              data: viewModel.tireDataWindow.data.map(t => t.leftRear),
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.graphContainer}>
+        <BaseLineGraph
+          minY={viewModel.tireDataWindow.min}
+          maxY={viewModel.tireDataWindow.max}
+          dataLength={viewModel.tireDataWindow.size}
+          title="Right Rear Tire Temperature"
+          data={[
+            {
+              label: 'Right Rear Tire Temp',
+              color: theme.colors.text.primary.onPrimary,
+              data: viewModel.tireDataWindow.data.map(t => t.rightRear),
+            },
+          ]}
+        />
+      </View>
+      {/* <LineGraph
         labelData={avgAxleLabels}
         getData={() => avgAxleData()}
       />
@@ -88,7 +172,7 @@ export function AvgTireTemps(props: AvgTireTempProps) {
       <LineGraph
         labelData={rightRearLabels}
         getData={() => getTireData('rightRear')}
-      />
+      /> */}
     </ScrollView>
   )
 }
@@ -97,6 +181,11 @@ function themeStyles() {
   return invokeWithTheme((theme) => StyleSheet.create({
     scrollView: {
       marginBottom: 60,
+    },
+    graphContainer: {
+      width: '100%',
+      height: 200,
+      marginBottom: 20,
     },
   }));
 }
